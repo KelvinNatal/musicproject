82ce91d1f3eb3f3344c190b0671cd27173cefaf3
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-08-13 16:51:22 -0300

    teste Readme

--- src/pages/PlayerPage/index.js
+++ src/pages/PlayerPage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import '../../main.css';
-import '../../input.css'
+import "../../main.css";
+import "../../input.css";
 import Header from "../../components/header";
 import Controls from "../../components/controls/Controls";
 import Actions from "../../components/playlist/Actions";
@@ -9,23 +9,20 @@ import Playlist from "../../components/playlist/Playlist";
 import PlayerState from "../../context/PlayerState";
 
 const PlayerPage = () => {
-    return (
-      <>
-          
-                <PlayerState>
-                <div className="corpo">
-                    <div className="">
-                        {/*<Header/>*/}
-                        <Actions/>
-                        <Playlist/>
-                    </div>
-                    <Controls/>
-                </div>
-                </PlayerState>
-          
-      </>   
-    );
-  }
-  
-  export default PlayerPage;
-  
+  return (
+    <>
+      <PlayerState>
+        <div className="corpo">
+          <div className="">
+            {/*<Header/>*/}
+            <Actions />
+            <Playlist />
+          </div>
+          <Controls />
+        </div>
+      </PlayerState>
+    </>
+  );
+};
+
+export default PlayerPage;
